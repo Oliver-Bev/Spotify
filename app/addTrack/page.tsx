bc708537ee1c10eaa70eecc815393db94f6a25c7
Oliver-Bev <oliver.p.szkola@gmail.com>
2025-12-01 22:58:59 +0100

    nwm2

--- app/addTrack/page.tsx
+++ app/addTrack/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+export const dynamic = 'force-dynamic';
 import { useState } from 'react';
 import { supabase } from '@/lib/supabaseClient';
 console.log('SUPABASE URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
